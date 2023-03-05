540b840af0f7fa5c7ac358536246b8c8112bc4a6
Mads <madeldelacruz@Madss-Mac-mini.local>
2023-03-05 23:26:23 +1300

    contact page is fully responsive.

--- sections/contact/hero.js
+++ sections/contact/hero.js
@@ -5,31 +5,31 @@ import Piano from "../../public/img/piano.jpg"
 function Hero() {
     return (
         <div>
-            <div className="grid grid-cols-2 pt-[18vh]">
-                <Image src={Piano} width={620} height={725} alt="image" className="rounded-3xl" />
-                <div className="font-rasfire text-[14vh] uppercase m-auto -space-y-24 tracking-[2px] text-dirt-brown">
+            <div className="grid sm:grid-cols-2 xxl:pt-[18vh] sm:pt-[15vh] pt-[20vh]">
+                <Image src={Piano} width={620} height={725} alt="image" className="rounded-3xl sm:block hidden"/>
+                <div className="font-rasfire md:text-[14vh] lm:text-[12vh] xxs:text-[10vh] text-[7vh] uppercase m-auto md:-space-y-24 lm:-space-y-20 xxs:-space-y-16 -space-y-10 tracking-[2px] text-dirt-brown">
                     <h1>Let's</h1>
                     <h1>Talk.</h1>
                 </div>
             </div>
-            <div className="mx-[5vw] my-[8vh] p-[2.5em] border-[1px] border-[#A5998E] rounded-2xl">
+            <div className="mx-[5vw] my-[8vh] xs:p-[2.5em] py-[2.5em] px-[5vw] border-[1px] border-[#A5998E] rounded-2xl">
                 <div className="text-center text-almost-black">
-                    <div className="font-rasfire text-[5.5vh] -space-y-10">
-                        <h2 className="ml-[4vw]">i'd love to get</h2>
-                        <h2 className="mr-[4vw]">to know you!</h2>
+                    <div className="font-rasfire sm:text-[5.5vh] xs:text-[5vh] xxs:text-[4.5vh] text-[3.5vh] xs:-space-y-10 xxs:-space-y-8 -space-y-5">
+                        <h2 className="xxs:ml-[4vw]">i'd love to get</h2>
+                        <h2 className="lg:mr-[4vw] md:mr-[6vw] lm:mr-[8vw] sm:mr-[10vw] xxs:mr-[15vw]">to know you!</h2>
                     </div>
-                    <p className="w-[35vw] m-auto font-thasadith text-[1.8vh] tracking-[0.5px]">
+                    <p className="md:w-[35vw] sm:w-[45vw] m-auto font-thasadith xs:text-[1.8vh] xxs:text-[1.6vh] text-[1.5vh] tracking-[0.5px]">
                         Lorem ipsum dolor sit amet consectetur. Egestas leo vitae ullamcorper blandit id. Habitant sed nibh penatibus ipsum nulla ut purus pulvinar nunc.
                     </p>
                 </div>
-                <div className="pt-[5vh] pb-[8vh]">
-                    <div className="flex justify-between pb-[3vh]">
+                <div className="sm:pt-[5vh] pt-[4vh] pb-[8vh]">
+                    <div className="lm:flex justify-between pb-[3vh] lm:space-y-0 space-y-5">
                         <input type="text" placeholder="Name" className="contact-textbox"/>
                         <input type="text" placeholder="Email" className="contact-textbox"/>
                     </div>
-                    <textarea type="text" placeholder="Message" className="w-full p-[1em] font-thasadith tracking-[1px] rounded-2xl h-[26vh]"/>
+                    <textarea type="text" placeholder="Message" className="w-full p-[1em] font-thasadith tracking-[1px] xxs:rounded-2xl rounded-md h-[26vh]"/>
                     <Link href="/">
-                        <button className="bg-dusty font-thasadith uppercase tracking-[2px] px-[3vw] py-[1.5vh] text-right rounded-xl text-off-white text-[2vh] mt-[2vh] float-right">
+                        <button className="bg-dusty font-thasadith uppercase tracking-[2px] sm:px-[3vw] xs:px-[8vw] px-[10vw] py-[1.5vh] max-xxs:w-full xxs:text-right text-center xxs:rounded-xl rounded-md text-off-white xs:text-[2vh] text-[1.8vh] mt-[2vh] float-right">
                             <h2>Submit</h2>
                         </button>
                     </Link>
