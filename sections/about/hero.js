dc0a7439534c1b59e23e81f5365f5d6f550f6e8e
Mads <madeldelacruz@Madss-Mac-mini.local>
2023-03-06 22:36:19 +1300

    added content to my about page. its getting real omg

--- sections/about/hero.js
+++ sections/about/hero.js
@@ -9,18 +9,18 @@ function Hero() {
                     <h1 className="font-rasfire xs:text-[8vh] xxs:text-[7vh] text-[5vh] uppercase text-almost-black">It's Me, Hi</h1>
                     <h2 className="font-thasadith xs:text-[2vh] xxs:text-[1.8vh] text-[1.6vh] xxs:tracking-[0.5px] tracking-[0.2px]">welcome to my little corner of the world. make yourself at home.</h2>
                 </div>
-                <div className="font-thasadith xs:text-[1.8vh] xxs:text-[1.6vh] text-[1.5vh] tracking-[0.5px] md:w-[40vw] space-y-5">
+                <div className="font-thasadith xs:text-[1.8vh] xxs:text-[1.6vh] text-[1.5vh] lg:tracking-[0.5px] tracking-[0.3px] md:w-[40vw] space-y-5">
                     <p>
                         can i interest you in some tea? i have a selection of three: jasmine, peppermint, or oolong. your selection will tell me something about who you are, so choose wisely. anyway, i'm glad you were able to find me! i'm sure that your journey has been long and tiring, so thank you for stopping by my little sanctuary. it may be small, but i'll tell you all about it.
                     </p>
                     <p>
-                        it's filled with the coziest of colours-- oaks, lavenders, embers and moss. there are tui's singing in the distance, a single cicada i keep in my jar and i ensure that it always smells of fresh linen, with the faintest smell of warm, baked chocolate chip cookies. there really aren't many things inside yet in terms of furnishings, but it is my dream to turn this small corner of mine into an entire world of my own, filled with all sorts of things-- some of them small, some of them big, but all of them wonderful. in order to do that, i've embarked on an perpetual quest for knowledge to deepen and sharpen my skills. not just in technology, but in all aspects of life; so i must apologise if you don't find me home some days. i have dragons to slay and damsels to rescue.
+                        it's filled with the coziest of colours-- oaks, lavenders, embers and moss. there are fireflies in my kitchen, tui's singing in the distance, a single cicada i keep in my jar and i ensure that it always smells of fresh linen, with the faintest smell of warm, baked chocolate chip cookies. there really aren't many things inside yet in terms of furnishings, but it is my dream to turn this small corner of mine into an entire world of my own, filled with all sorts of things-- some of them small, some of them big, but all of them wonderful. in order to do that, i've embarked on an perpetual quest for knowledge to deepen and sharpen my skills, not just in technology, but in all aspects of life; so i must apologise if you don't find me home some days. i have dragons to slay and damsels to rescue.
                     </p>
                     <p>
-                        in the "real" world, i live in the land down under-- the new land of zea-- in a small house, inside a small village under a cloudy mountain with my sister and four cats. i may live here physically, but i live in my head most days. i like art, i like writing, i like books, i like music, i like animals. i guess those are the big things about me. i'm constantly on-the-go, always planning and seeking something new-- but when i fall in love (with anything!), i am relentless. i sit down, i absorb myself in the work and i stick with it obsessively. i never stop until i'm done. i'm good at the things i choose to do. most of it, anyway. or so i'd like to think.                
+                        in the "real" world, i live in the land down under-- the new land of zea-- in a small house, inside a small village under a cloudy mountain with my sister and four cats. i may live here physically, but i live in my head most days. i like art, i like writing, i like books, i like music, i like animals. i guess those are the big things about me. i'm constantly on-the-go, always planning and seeking something new-- but when i fall in love with something, i am relentless. i sit down, i absorb myself in the work and i stick with it obsessively. i never stop until i'm done. i'm good at the things i choose to do. most of it, anyway. or so i'd like to think.                
                     </p>
                     <p>
-                        i apologize, weary traveller, if i've exhausted you with all of this speak. there are many more things i'd like to say but i'd rather not burden you when you have yet so long to go. i'm sure you'd like to see the rest of this small digital space i call my home! feel free to peruse, to browse, and should you have any questions, please don't hesitate to ask. oh, and finish your tea before you leave :)
+                        i apologize, weary traveller, if i've exhausted you with all this speak. there are many more things i'd like to say but i'd rather not burden you when you have yet so long to go. i'm sure you'd like to see the rest of this small digital space i call my home! feel free to peruse, and should you have any questions, please don't hesitate to ask. oh, and finish your tea before you leave :)
                     </p>
                     <p>
                         'til we meet again, <br/>
@@ -28,7 +28,7 @@ function Hero() {
                     </p>
                 </div>
             </div>
-            <div className="pr-[2vw] sm:pr-0">
+            <div className="pr-[2vw] sm:pr-0 m-auto">
                 <Image src={AboutOne} width={585} height={535} alt="image" className="rounded-3xl max-xl:pr-[2vw]"/>
             </div>
         </div>
--- sections/about/pitch.js
+++ sections/about/pitch.js
@@ -3,24 +3,24 @@ import AboutTwo from "../../public/img/about02.png"
 
 function Pitch() {
     return (
-        <div className="bg-[#78736E] xs:py-[15vh] py-[10vh] xl:px-[10vw] px-[6.5vw]">
+        <div className="bg-[#78736E] xs:py-[12vh] py-[10vh] xl:px-[10vw] px-[6.5vw]">
             <div>
                 <div className="md:grid md:grid-cols-3 flex flex-col-reverse">
                     <div className="m-auto md:block lm:flex">
-                        <div className="bg-black text-off-white font-thasadith xs:text-[1.8vh] text-[1.6vh] sm:pl-[5vw] sm:pr-[10vw] px-[8vw] lg:py-[10vh] sm:py-[8vh] xxs:py-[6vh] py-[5vh] rounded-3xl md:mb-0 sm:mb-[10vh] xs:mb-[5vh] mb-[4vh]">
+                        <div className="bg-black text-off-white font-thasadith xs:text-[1.8vh] text-[1.6vh] sm:pl-[5vw] sm:pr-[10vw] px-[8vw] lg:py-[9vh] sm:py-[8vh] xxs:py-[6vh] py-[5vh] rounded-3xl md:mb-0 sm:mb-[10vh] xs:mb-[5vh] mb-[4vh]">
                             <div className="md:w-[30vw] lm:w-[50vw] xs:space-y-8 space-y-5 tracking-[0.5px]">
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur. In non in tortor commodo commodo imperdiet mi sit. Molestie orci egestas egestas felis tellus diam lectus. Integer dictumst quam nec nisl ornare mi.
+                                    I like to think working with me is very easy. My collaboration style is very heavy on listening, I pay close attention and take on board all things that are said-- whether it be critique, ideas or analyses. I look for patterns in everything I see and when I can afford to do so, I enjoy refining things right down to the atom. I like to find ways to integrate the principles, ideas, techniques and skills I've gained from mastering certain crafts to infuse into my work in order to create something beautiful and interesting.
                                 </p>
                                 <p>
-                                    A elit diam molestie purus in sodales lacus turpis nibh. Dictum velit consequat dictum rhoncus dictumst sed tristique. Sed id enim porttitor interdum vivamus imperdiet purus.
+                                    I've been rewarded some rich experiences in a few different roles in different industries throughout my secular lifetime. I've been in customer service, fast food, research, call centers and sales, but none of them have enraptured me as much as tech. My curse is that I am both creative and analytic-- two sides of me that are in eternal conflict. However, technology has allowed me to build a bridge to close the gap between the two, helping me find joy and fulfilment in my work.
                                 </p>
                                 <p>
-                                    Sit urna interdum fringilla tempor eget tellus. Orci lorem suspendisse nulla nulla lobortis ultrices urna a ac. Eu pulvinar laoreet odio lectus tellus porttitor urna habitasse. Sed est senectus est turpis. Faucibus tellus porta enim massa convallis.
+                                    In my experiences, I've learned many things. But I hold strongly to three particular values and use them as a guiding light in all areas of my work, because I believe that they are the keys to push myself and those around me to success.
                                 </p>
                             </div>
                         </div>
-                        <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl lm:-ml-[8vw] mt-[15vh] md:hidden max-lm:hidden"/>
+                        <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl lm:-ml-[8vw] mt-[15vh] md:hidden max-lm:hidden" />
                     </div>
                     <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl ml-[12vw] mt-[25vh] md:block hidden" />
                     <div className="text-off-white md:text-right md:pr-0 sm:pr-[25vw] xs:pr-[15vw] md:pb-0 pb-[5vh] space-y-2 relative">
@@ -32,19 +32,22 @@ function Pitch() {
                     <div className="about-pitch-div">
                         <h1 className="about-pitch-num">01</h1>
                         <p className="about-pitch-p">
-                            Sit urna interdum fringilla tempor eget tellus. Orci lorem suspendisse nulla nulla lobortis ultrices urna a ac. Eu pulvinar laoreet odio lectus tellus porttitor urna habitasse. Sed est senectus est turpis. Faucibus tellus porta enim massa convallis.
+                            <span className="italic">Professionalism.</span><br />
+                            I never want a client, a customer, a partner or even a friend to walk away from any conversation with me feeling downcast or slighted. I pay very close attention to the words I speak and the way that it's delivered. I like to be concise and direct, for collaboration and connection is the pinnacle of a work environment that nurtures growth, and I believe the work that we produce will be a direct reflection of that.
                         </p>
                     </div>
                     <div className="about-pitch-div">
                         <h1 className="about-pitch-num">02</h1>
                         <p className="about-pitch-p">
-                            A elit diam molestie purus in sodales lacus turpis nibh. Dictum velit consequat dictum rhoncus dictumst sed tristique. Sed id enim porttitor interdum vivamus imperdiet purus.
+                            <span className="italic">Honesty and Creative Integrity.</span><br />
+                            All art forms, to me, are incredibly sacred. And all of my work must be bound by virtues if they are to be worth anything. More than that, the ability to give and take valid criticism is something I pride myself on. Being honest is one of the biggest ways I offer value.
                         </p>
                     </div>
                     <div className="about-pitch-div">
                         <h1 className="about-pitch-num">03</h1>
                         <p className="about-pitch-p">
-                            Lorem ipsum dolor sit amet consectetur. In non in tortor commodo commodo imperdiet mi sit. Molestie orci egestas egestas felis tellus diam lectus. Integer dictumst quam nec nisl ornare mi.
+                            <span className="italic">Balance.</span><br />
+                            It is necessary for me to derive fulfilment from many different avenues in life-- not just the secular nor the recreational. Doing so allows room for ideas and inspiration to manifest, for creativity to grow and for my art to thrive. I can give my best in everything, if I am balanced in everything.
                         </p>
                     </div>
                 </div>
