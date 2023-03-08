a9c42aa525ac083a604daf6410cd00cf10561fc5
Mads <madeldelacruz@Madss-Mac-mini.local>
2023-03-08 14:02:15 +1300

    added contentgit add .

--- sections/about/pitch.js
+++ sections/about/pitch.js
@@ -7,28 +7,27 @@ function Pitch() {
             <div>
                 <div className="md:grid md:grid-cols-3 flex flex-col-reverse">
                     <div className="m-auto md:block lm:flex">
-                        <div className="bg-black text-off-white font-thasadith xs:text-[1.8vh] text-[1.6vh] sm:pl-[5vw] sm:pr-[10vw] px-[8vw] lg:py-[9vh] sm:py-[8vh] xxs:py-[6vh] py-[5vh] rounded-3xl md:mb-0 sm:mb-[10vh] xs:mb-[5vh] mb-[4vh]">
-                            <div className="md:w-[30vw] lm:w-[50vw] xs:space-y-8 space-y-5 tracking-[0.5px]">
+                        <div className="bg-black text-off-white font-thasadith xs:text-[1.8vh] text-[1.6vh] sm:pl-[5vw] sm:pr-[10vw] px-[8vw] lg:py-[9vh] xxs:py-[6vh] py-[5vh] rounded-3xl lm:mb-0 mb-[5vh]">
+                            <div className="md:w-[30vw] xs:space-y-8 space-y-5 tracking-[0.5px]">
                                 <p>
-                                    I like to think working with me is very easy. My collaboration style is very heavy on listening, I pay close attention and take on board all things that are said-- whether it be critique, ideas or analyses. I look for patterns in everything I see and when I can afford to do so, I enjoy refining things right down to the atom. I like to find ways to integrate the principles, ideas, techniques and skills I've gained from mastering certain crafts to infuse into my work in order to create something beautiful and interesting.
+                                    I like to think working with me is easy. My collaboration style is very heavy on listening. I pay close attention and take on board all things that are said-- whether it be critique, ideas or analyses. I look for patterns in everything I see and when I can afford to do so, I enjoy refining things right down to the atom. I like to find ways to integrate the principles, ideas, techniques and skills I've gained from mastering certain crafts to infuse into my work in order to create something beautiful, unique and interesting.
                                 </p>
                                 <p>
-                                    I've been rewarded some rich experiences in a few different roles in different industries throughout my secular lifetime. I've been in customer service, fast food, research, call centers and sales, but none of them have enraptured me as much as tech. My curse is that I am both creative and analytic-- two sides of me that are in eternal conflict. However, technology has allowed me to build a bridge to close the gap between the two, helping me find joy and fulfilment in my work.
+                                    I've been rewarded some rich experiences inside a few different roles in different industries throughout my secular lifetime. I've been in customer service, fast food, research, call centers and sales, but none of them have enraptured me as much as tech. My curse is that I am both creative and analytic-- two sides of me that are in eternal conflict. However, technology has allowed me to build a bridge to close the gap between the two, helping me find joy and fulfilment in my work.
                                 </p>
                                 <p>
-                                    In my experiences, I've learned many things. But I hold strongly to three particular values and use them as a guiding light in all areas of my work, because I believe that they are the keys to push myself and those around me to success.
+                                    In my experiences, I've learned many things. However, I hold strongly to three particular values and use them as a guiding light in all areas of my work, because I believe that they are the keys to push myself and those around me to success.
                                 </p>
                             </div>
                         </div>
-                        <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl lm:-ml-[8vw] mt-[15vh] md:hidden max-lm:hidden" />
                     </div>
-                    <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl ml-[12vw] mt-[25vh] md:block hidden" />
+                    <Image src={AboutTwo} width={305} height={420} alt="image" className="rounded-3xl ml-[12vw] mt-[35vh] md:block hidden" />
                     <div className="text-off-white md:text-right md:pr-0 sm:pr-[25vw] xs:pr-[15vw] md:pb-0 pb-[5vh] space-y-2 relative">
                         <h2 className="font-thasadith xxs:text-[2vh] text-[1.8vh] tracking-[0.5px] opacity-80">but let&apos;s bring ourselves back down to earth.</h2>
                         <h1 className="font-rasfire uppercase xl:text-[7vh] xs:text-[6vh] xxs:text-[5.5vh] text-[4.5vh] leading-none opacity-90">What&apos;s it Like to work with me?</h1>
                     </div>
                 </div>
-                <div className="sm:flex sm:place-content-between lm:mt-[10vh] sm:space-y-0 space-y-8 sm:px-0 px-[2vw]">
+                <div className="lm:flex sm:place-content-between lm:mt-[10vh] lm:space-y-0 space-y-8 lm:px-0 px-[2vw]">
                     <div className="about-pitch-div">
                         <h1 className="about-pitch-num">01</h1>
                         <p className="about-pitch-p">
--- sections/projects/project-items.js
+++ sections/projects/project-items.js
@@ -1,31 +1,31 @@
 function ProjectsItems() {
     return (        
             <div className="lm:pt-[25vh] pt-[18vh] sm:pb-[10vh] px-[5vw] text-off-white xs:space-y-16 space-y-10">
-                <div className="bg-vinorepo bg-cover bg-center project-div">
+                <div className="bg-vinorepo bg-cover bg-center bg-fixed project-div">
                     <div className="project-div-header">
                         <h2 className="project-div-subheading">Project One</h2>
                         <h1 className="project-div-title">vinorepo</h1>
                     </div>
                     <div className="project-div-p">
                         <p>
-                            Lorem ipsum dolor sit amet consectetur. Egestas leo vitae ullamcorper blandit id. Habitant sed nibh penatibus ipsum nulla ut purus pulvinar nunc.
+                            Ever had a fine bottle of wine and wanted to know what it's story was? Each vintage has a story, starting from roots of the crops, all the way to your wineglass. Vinorepo hopes to connect you and that story, making for a more intimate wine drinking experience.
                         </p>
                         <p>
-                            Natoque orci lorem sit venenatis adipiscing. Orci purus dui ultrices nisi tincidunt. In sed vitae ut purus sit. Vestibulum velit dignissim hendrerit commodo ornare pharetra sit risus feugiat. Quis malesuada sem nunc dolor.
+                            In this project, I am in the process of creating a platform for vineyards and wine enthusiasts. I am privileged to be able to work for Kerry and bring his idea to life, as I get to play with different design concepts, integrate some cutting-edge technology and learn more about wine! 
                         </p>
                     </div>
                 </div>
-                <div className="bg-neubrandz bg-cover bg-center project-div lm:text-right">
+                <div className="bg-neubrandz bg-cover bg-center bg-fixed project-div lm:text-right">
                     <div className="project-div-header">
                         <h2 className="project-div-subheading">Project Two</h2>
                         <h1 className="project-div-title">neubrandz</h1>
                     </div>
                     <div className="project-div-p lm:text-right lm:float-right">
                         <p>
-                            Lorem ipsum dolor sit amet consectetur. Egestas leo vitae ullamcorper blandit id. Habitant sed nibh penatibus ipsum nulla ut purus pulvinar nunc.
+                            Health is our wealth. Neubrandz is an exciting startup company that focuses on consumables targeted at advancing health using organic products locally found in the Philippines.
                         </p>
                         <p>
-                            Natoque orci lorem sit venenatis adipiscing. Orci purus dui ultrices nisi tincidunt. In sed vitae ut purus sit. Vestibulum velit dignissim hendrerit commodo ornare pharetra sit risus feugiat. Quis malesuada sem nunc dolor.
+                            In this project, I had the privilege of working alongside Sir Delfin and his team in designing and developing their website. I also enjoyed seeing the growth of their company and the popularity of their products increase through the year. 
                         </p>
                     </div>
                 </div>
