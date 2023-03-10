3c74d5282ecfb0d9645bf8dae71970c90886cf69
Mads <madeldelacruz@Madss-Mac-mini.local>
2023-03-10 20:56:50 +1300

    added router to index page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,19 @@ import Intro from "@/sections/home/intro";
 import Portfolio from "@/sections/home/portfolio";
 import Slogan from "@/sections/home/slogan";
 import Header from "@/components/layout/header";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
+
+  console.log(`query = ${router.query}`)
+  console.log(`pathname = ${router.pathname}`)
+  console.log(router)
+
+  if (router.asPath !== "/") {
+    router.push(router.asPath);
+  }
+  
   return (
     <div>
       <Header />
